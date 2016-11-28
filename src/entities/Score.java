c4a090ffb4e2b989a1bcfc1c2e36ac326d0c5345
Tom <Tom@130.215.250.186>
2016-11-28 18:35:52 -0500

    score

--- src/entities/Score.java
+++ src/entities/Score.java
@@ -1,5 +1,20 @@
 package entities;
 
 public class Score {
+	int value;
+	boolean star1Filled;
 
+	boolean star2Filled;
+
+	boolean star3Filled;
+
+	public Score setScore(int i) {
+		value = i;
+		return this;
+	}
+
+	public Score addToScore(int scoreAdded) {
+		setScore(value + scoreAdded);
+		return this;
+	}
 }
