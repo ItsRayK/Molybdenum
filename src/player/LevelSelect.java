12edb7ea04baf78480f51d37cf200e299709c355
ItsRayK <xboxer221@live.com>
2016-11-29 00:09:39 -0500

    attempt 3

--- src/player/LevelSelect.java
+++ src/player/LevelSelect.java
@@ -3,20 +3,23 @@ package player;
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
 
-import javax.swing.*;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
-
-import java.awt.*;
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import javax.swing.SwingConstants;
+import java.awt.Font;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class LevelSelect extends JFrame {
 
 	private JPanel contentPane;
 	private JButton btnLevel1;
-	private JButton btnLevel2;
-	private JButton btnLevel3;
-
 
 	/**
 	 * Launch the application.
@@ -44,9 +47,17 @@ public class LevelSelect extends JFrame {
 		initializeController();
 	}
 
+	private void initializeModel() {
+
+		btnLevel1 = new JButton("Level 1");
+
+
+	}
+
 	private void initializeView() {
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		setBounds(100, 100, 1000, 570);
+		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
@@ -55,30 +66,23 @@ public class LevelSelect extends JFrame {
 		btnLevel1.setBounds(259, 155, 80, 80);
 		contentPane.add(btnLevel1);
 		
-	}
 
-	private void initializeModel() {
+		JLabel bg = new JLabel("");
+		bg.setIcon(new ImageIcon(player.LevelSelect.class.getResource("/images/BackgroundTitle.gif")));
+		bg.setBounds(0, 0, 994, 531);
+		contentPane.add(bg);
 		
-		contentPane = new JPanel();
-		
-		
-		btnLevel1 = new JButton("Level 1");
-		
-		
-		JLabel lblNewLabel = new JLabel("New label");
-		lblNewLabel.setIcon(new ImageIcon(LevelSelect.class.getResource("/images/BackgroundTitle.gif")));
-		lblNewLabel.setBounds(0, 0, 994, 531);
-		contentPane.add(lblNewLabel);
 		
 	}
 
 	private void initializeController() {
 		btnLevel1.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
+			public void actionPerformed(ActionEvent arg0) {
 				PuzzleView temp = new PuzzleView();
 				temp.setVisible(true);
 				dispose();
 			}
 		});
+
 	}
 }
