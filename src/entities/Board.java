c59c2383b4c96400441a006721f2140808b98771
Tom Hagen <Tom Hagen@130.215.221.130>
2016-11-30 14:49:13 -0500

    test to see if i come up as a contributor

--- src/entities/Board.java
+++ src/entities/Board.java
@@ -2,8 +2,8 @@ package entities;
 
 public class Board {
 	Square[][] squares = new Square[6][6];
-//test to see if i show up as a contributor
-	
+
+
 	public Square[][] activeSquares() {
 		Square[][] activeOnes = new Square[6][6];
 		for(int i = 1; i<=6; i++){
