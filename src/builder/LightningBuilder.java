77791455919cf669b50d8adbc59da8b6d7b919a1
Tom <Tom@130.215.250.186>
2016-12-01 23:30:31 -0500

    lightning preview

--- src/builder/LightningBuilder.java
+++ src/builder/LightningBuilder.java
@@ -7,8 +7,11 @@ import java.awt.event.ActionListener;
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 
+import entities.Board;
 import entities.Letter;
-
+import entities.Lightning;
+import player.LightningView;
+import player.PuzzleView;
 
 public class LightningBuilder extends JFrame {
 
@@ -20,7 +23,9 @@ public class LightningBuilder extends JFrame {
 	private JTextField txtlevelName;
 	private JButton btnBack, btnPreview, btnDelete, btnSaveLevel;
 	private JLabel lblScoreThresholds, lblLevelTypeLightning;
-	private JCheckBox checkBox[][];
+	private JCheckBox checkBox[][] = new JCheckBox[6][6];
+	int i, j;
+	Lightning lightning = new Lightning("default", new Board());
 
 	/**
 	 * Launch the application.
@@ -44,13 +49,13 @@ public class LightningBuilder extends JFrame {
 	public LightningBuilder() {
 		initialize();
 	}
-	
+
 	private void initialize() {
 		initializeModel();
 		initializeView();
 		initializeController();
 	}
-	
+
 	private void initializeModel() {
 		setResizable(false);
 		setTitle("Letter Craze Builder");
@@ -60,7 +65,7 @@ public class LightningBuilder extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
+
 		lblScoreThresholds = new JLabel("Score Thresholds:");
 		txt1StarThresh = new JTextField();
 		txt2StarThresh = new JTextField();
@@ -76,85 +81,81 @@ public class LightningBuilder extends JFrame {
 		btnDelete = new JButton("Delete");
 
 	}
-		
-	private void initializeView(){
+
+	private void initializeView() {
 		for (int i = 0; i <= 5; i++) {
 			for (int j = 0; j <= 5; j++) {
-				Letter l = new Letter();
-				l.randomLetter();
 
-				checkBox = new JCheckBox[6][6];
-				checkBox[i][j] = new JCheckBox("");
+				checkBox[i][j] = new JCheckBox("", true);
 				checkBox[i][j].setBounds(534 + i * 66, 91 + j * 66, 20, 20);
 				contentPane.add(checkBox[i][j]);
 
 			}
 
 		}
-		
-		
+
 		lblScoreThresholds.setBounds(172, 154, 120, 14);
 		contentPane.add(lblScoreThresholds);
-		
+
 		txt1StarThresh.setText("(Score required for 1 star)");
 		txt1StarThresh.setColumns(10);
 		txt1StarThresh.setBounds(172, 170, 171, 20);
 		contentPane.add(txt1StarThresh);
-		
-		
+
 		txt2StarThresh.setText("(Score required for 2 stars)");
 		txt2StarThresh.setColumns(10);
 		txt2StarThresh.setBounds(172, 195, 171, 20);
 		contentPane.add(txt2StarThresh);
-		
+
 		txt3StarThresh.setText("(Score required for 3 stars)");
 		txt3StarThresh.setColumns(10);
 		txt3StarThresh.setBounds(172, 220, 171, 20);
 		contentPane.add(txt3StarThresh);
-		
+
 		txtlevelName.setText("(Level Name)");
 		txtlevelName.setBounds(10, 126, 95, 20);
 		contentPane.add(txtlevelName);
 		txtlevelName.setColumns(10);
-		
+
 		lblLevelTypeLightning.setHorizontalAlignment(SwingConstants.CENTER);
 		lblLevelTypeLightning.setFont(new Font("Tahoma", Font.PLAIN, 20));
 		lblLevelTypeLightning.setBounds(10, 86, 510, 32);
 		contentPane.add(lblLevelTypeLightning);
-		
+
 		JLabel gridImg = new JLabel("");
 		gridImg.setIcon(new ImageIcon(PuzzleBuilder.class.getResource("/images/Grid.gif")));
 		gridImg.setBounds(530, 86, 400, 400);
 		contentPane.add(gridImg);
-				
+
 		JLabel lblTime = new JLabel("Timer:");
 		lblTime.setBounds(172, 129, 37, 14);
 		contentPane.add(lblTime);
-		
+
 		txtSetTime.setText("(Set Time)");
 		txtSetTime.setBounds(217, 126, 86, 20);
 		contentPane.add(txtSetTime);
 		txtSetTime.setColumns(10);
-		
+
 		btnSaveLevel.setBounds(10, 95, 95, 23);
 		contentPane.add(btnSaveLevel);
-		
+
 		btnBack.setBounds(10, 161, 95, 23);
 		contentPane.add(btnBack);
 
 		btnPreview.setBounds(10, 190, 95, 23);
 		contentPane.add(btnPreview);
-		
+
 		btnDelete.setBounds(10, 219, 95, 23);
 		contentPane.add(btnDelete);
-		
+
 		JLabel bg = new JLabel("");
 		bg.setIcon(new ImageIcon(LightningBuilder.class.getResource("/images/BackgroundBlank.gif")));
 		bg.setBounds(0, 0, 994, 541);
 		contentPane.add(bg);
 	}
-	
-	private void initializeController(){
+
+	private void initializeController() {
+
 		btnBack.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				Builder mainMenu = new Builder();
@@ -162,6 +163,20 @@ public class LightningBuilder extends JFrame {
 				dispose();
 			}
 		});
+
+		btnPreview.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				for (i = 0; i <= 5; i++) {
+					for (j = 0; j <= 5; j++) {
+						if (checkBox[i][j].isSelected())
+							lightning.getBoard().activateSquare(i, j);
+						else
+							lightning.getBoard().deActivateSquare(i, j);
+					}
+				}
+				LightningView lightningView = new LightningView(txtlevelName.getText(), lightning);
+				lightningView.setVisible(true);
+			}
+		});
 	}
-	
 }
