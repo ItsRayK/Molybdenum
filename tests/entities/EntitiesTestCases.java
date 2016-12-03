c8e62eb47fd62116cf36f54f01b14fd9a5828f98
thagen15 <thagen@wpi.edu>
2016-12-03 17:28:28 -0500

    Test case folder and packages

---
+++ tests/entities/EntitiesTestCases.java
@@ -0,0 +1,14 @@
+package entities;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class EntitiesTestCases {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
