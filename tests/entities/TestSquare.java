4fe6ef7785d3779831da30a866c94d75e99f1b3e
rachelhahn <rj.hahn97@gmail.com>
2016-12-08 14:42:08 -0500

    Testing Fun

--- tests/entities/TestSquare.java
+++ tests/entities/TestSquare.java
@@ -18,5 +18,43 @@ public class TestSquare extends TestCase {
 		
 		assertTrue (s.getContentsString() == null);   // AWKWARD
 	}
+	
+	public void testRandom() {
+		Square s = new Square (0, 0, true);
+		s.fillSquareWithRandom();
+		assertTrue (s.notEmptyActive());
+		
+		s.setActivity(false);
+		assertFalse(s.notEmptyActive());
+		
+		s.setActivity(true);
+		
+		Square s2 = new Square (0, 0, true);
+		assertTrue(s.isSameSquare(s2));
+		
+		Square s3 = new Square (0, 2, true);
+		assertFalse(s.isSameSquare(s3));
+	}
+	
+	public void testAdjacent() {
+		Square s = new Square (2, 2, true);
+		Square s2 = new Square (2, 1, true);
+		Square s3 = new Square (1, 2, true);
+		Square s4 = new Square (1, 1, true);
+		Square s5 = new Square (3, 3, true);
+		Square s6 = new Square (3, 2, true);
+		Square s7 = new Square (3, 1, true);
+		Square s8 = new Square (2, 3, true);
+		Square s9 = new Square (1, 3, true);
+		
+		assertTrue(s.isAdjacentTo(s2));
+		assertTrue(s.isAdjacentTo(s3));
+		assertTrue(s.isAdjacentTo(s4));
+		assertTrue(s.isAdjacentTo(s5));
+		assertTrue(s.isAdjacentTo(s6));
+		assertTrue(s.isAdjacentTo(s7));
+		assertTrue(s.isAdjacentTo(s8));
+		assertTrue(s.isAdjacentTo(s9));
+	}
 
 }
